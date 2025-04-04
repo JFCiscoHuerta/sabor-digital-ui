9fab096bef8aec8afa1e873660fa781e3f2515e5
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2025-04-03 20:05:54 -0600

    Add Waiter Path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,4 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: "restaurant", loadChildren: () => import('./features/restaurant/restaurant.module').then(m => m.RestaurantModule) },
+  { path: "waiter", loadChildren: () => import('./features/waiter/waiter.module').then(m => m.WaiterModule) },
 ];
