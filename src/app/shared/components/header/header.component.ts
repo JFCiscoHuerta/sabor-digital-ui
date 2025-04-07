d623ab08880c57e9607f030f98326607aa4b2b7b
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2025-04-06 22:02:45 -0600

    Add Header Design

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    RouterLink
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
